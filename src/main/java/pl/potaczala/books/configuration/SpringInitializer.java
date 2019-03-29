c9bd2701ee591e40fe8a936c11e03d25a033847a
gpotaczala <grzegorz.potaczala@softres.pl>
2019-03-29 10:28:57 +0100

    Ustawione kodowanie UTF-8 w springu - bean w SpringInitializer

--- src/main/java/pl/potaczala/books/configuration/SpringInitializer.java
+++ src/main/java/pl/potaczala/books/configuration/SpringInitializer.java
@@ -1,5 +1,8 @@
 package pl.potaczala.books.configuration;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class SpringInitializer extends AbstractAnnotationConfigDispatcherServletInitializer  { 
@@ -18,4 +21,11 @@ public class SpringInitializer extends AbstractAnnotationConfigDispatcherServlet
 	protected String[] getServletMappings() { 
 		return new String[]{"/"}; 
 	} 
+	
+    @Override
+    protected Filter[] getServletFilters() {
+      CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+      characterEncodingFilter.setEncoding("UTF-8");
+      return new Filter[] { characterEncodingFilter};
+    }	
 }
