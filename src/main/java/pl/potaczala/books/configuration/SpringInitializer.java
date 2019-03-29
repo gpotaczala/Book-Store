aca383facaa6cccbb625bbff439ce8fd417c14e8
gpotaczala <grzegorz.potaczala@softres.pl>
2019-03-29 09:12:18 +0100

    Konfiguracja aplikacji przerobiona w całości z xml'i na adnotacje

---
+++ src/main/java/pl/potaczala/books/configuration/SpringInitializer.java
@@ -0,0 +1,21 @@
+package pl.potaczala.books.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringInitializer extends AbstractAnnotationConfigDispatcherServletInitializer  { 
+	
+	@Override 
+	protected Class<?>[] getRootConfigClasses() { 
+		return new Class[] {ApplicationConfig.class}; //na przysz³oœæ - na razie ApplicatnionConfig
+	} 
+	
+	@Override 
+	protected Class<?>[] getServletConfigClasses() { 
+		return new Class[] {ApplicationConfig.class}; 
+	} 
+	
+	@Override 
+	protected String[] getServletMappings() { 
+		return new String[]{"/"}; 
+	} 
+}
